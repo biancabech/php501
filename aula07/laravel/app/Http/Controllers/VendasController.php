3a9a8320eb9ddf5d4bd6564d4fa0a1b89f00d1f5
biancabech <biancapaulabech@gmail.com>
2019-11-22 23:09:52 -0200

    aula08

---
+++ aula07/laravel/app/Http/Controllers/VendasController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VendasController extends Controller
+{
+    public function index()
+    {
+        $controller = 'Vendas Controller';
+        return view('vendas', ['controller', $controller]);
+    }   
+}
---
+++ aula08/calculadora.php
@@ -0,0 +1,18 @@
+<?php
+class Calculadora {
+    public function somar($n1, $n2){
+        return $n1+$n2 ;
+    }
+
+    public function subtrair(){
+        return $n1-$n2 ;
+    }
+
+    public function multiplicar(){
+        return $n1*$n2 ;
+    }
+
+    public function dividir(){
+        return $n1/$n2 ;
+    }
+}
